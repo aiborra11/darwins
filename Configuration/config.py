7a9c8bad61c081f4cb98e415c4ec0754c67c4044
aiborra <iborra.11@gmail.com>
2021-02-21 22:39:26 +0100

    features calculator done

--- Configuration/config.py
+++ Configuration/config.py
@@ -1,16 +1,19 @@
 class Config:
-    CANDLES_PATH = 'Data/darwinex/trainTimeSeries/trainTimeSeries/TrainCandles'
-    SCORES_PATH = 'Data/darwinex/trainTimeSeries/trainTimeSeries/TrainScores'
+    CANDLES_PATH: str = 'Data/darwinex/trainTimeSeries/trainTimeSeries/TrainCandles'
+    SCORES_PATH: str = 'Data/darwinex/trainTimeSeries/trainTimeSeries/TrainScores'
 
-    CANDLES_DATA = True
-    SCORES_DATA = True
+    CANDLES_DATA: bool = True
+    SCORES_DATA: bool = True
 
-    VALID_DARWINS = ["REU", "VRT", "EEY", "JTL", "SEH", "BSX", "OJG", "UEI", "HQU", "ZXW", "LEN", "YEC",
-                     "UYZ", "LWK", "ACY", "HEO", "FIR", "BGN"]
+    VALID_DARWINS: list = ["REU", "VRT", "EEY", "JTL", "SEH", "BSX", "OJG", "UEI", "HQU", "ZXW", "LEN", "YEC",
+                           "UYZ", "LWK", "ACY", "HEO", "FIR", "BGN"]
 
-    TRAIN_SIZE = 0.8
-    LOG_COLS = ['close', 'max', 'min', 'open']
+    TRAIN_SIZE: float = 0.8
+    LOG_COLS: list = ['close', 'max', 'min', 'open']
 
-    TIMEFRAME = '1H'
+    TIMEFRAME: str = '1H'
+
+    OPERATING_MARKETS: bool = True
+    TECHNICAL_INDICATORS: bool = True
 
 config=Config()
